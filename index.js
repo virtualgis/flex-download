ca44154950ef94c43ca297b5e47de291135ab809
Piero Toffanin <pt@masseranolabs.com>
2018-06-21 14:09:04 -0400

    Removed test line

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const flexDownload = require('./flex-download');
 module.exports = flexDownload;
 
-flexDownload.download("https://maps.co.madison.il.us/", 'downloads', true).then(function(){
-    console.log("Done!");
-});
